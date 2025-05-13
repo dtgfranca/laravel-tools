3681d20fb6de6f7d403c5ff659c0f68374b521f9
Diego França <diego.lightbase@sebraemg.com.br>
2025-05-13 19:34:19 -0300

    adiciona a atualizacao para o laravel 10

--- atualiza_laravel10.sh
+++ atualiza_laravel10.sh
@@ -22,14 +22,35 @@ mkdir -p $BACKUP_DIR
 cp composer.json composer.lock $BACKUP_DIR/
 cp -r app config routes $BACKUP_DIR/
 
+
 # Atualizando versão do Laravel no composer.json
 echo "📦 Atualizando composer.json..."
-sed -i 's/"laravel\/framework": "[^"]*"/"laravel\/framework": "^12.0"/' composer.json
-sed -i 's/"phpunit\/phpunit": "[^"]*"/"phpunit\/phpunit": "^11.0"/' composer.json
-sed -i 's/"tallstackui\/tallstackui": "[^"]*"/"tallstackui\/tallstackui": "^2.5.2"/' composer.json
-sed -i 's/"pestphp\/pest": "[^"]*"/"pestphp\/pest": "^12.0"/' composer.json
-sed -i 's/"php": "[^"]*"/"php": "^8.4"/' composer.json
+sed -i 's/"laravel\/framework": "[^"]*"/"laravel\/framework": "^10.0"/' composer.json
+sed -i 's/"laravel\/sanctum": "[^"]*"/"laravel\/sanctum": "^3.2"/' composer.json
+sed -i 's/"laravel\/passport": "[^"]*"/"laravel\/passport": "^11.0"/' composer.json
+sed -i 's/"laravel\/ui": "[^"]*"/"laravel\/ui": "^4.0"/' composer.json
+sed -i 's/"doctrine\/dbal": "[^"]*"/"doctrine\/dbal": "^3.0"/' composer.json
+sed -i 's/"spatie\/laravel-ignition": "[^"]*"/"spatie\/laravel-ignition": "^2.0"/' composer.json
+sed -i 's/"php": "[^"]*"/"php": "^8.1"/' composer.json
 
+echo "A atualização para o phpunit 10 é opcional. Você gostaria de atualizar para o phpunit 10? (s/n)"
+read resposta
+
+
+if [ "$resposta" = "s" ]; then
+    echo "Atualizando para phpunit 10..."
+    # coloque aqui o comando de atualização
+    sed -i 's/"nunomaduro\/collision": "[^"]*"/"nunomaduro\/collision": "^7.0"/' composer.json
+    sed -i 's/"phpunit\/phpunit": "[^"]*"/"phpunit\/phpunit": "^10.0"/' composer.json
+    sed -i 's/\(<coverage[^>]*\) processUncoveredFiles="[^"]*"/\1/' phpunit.xml
+
+else
+    echo "Atualização cancelada."
+fi
+
+# Update de dependências
+echo "📦 atualizando composer ..."
+composer self-update --2
 
 # Update de dependências
 echo "📦 Rodando composer update..."
